6fd3f4348b55a049a607cbed755c86489582afd5
KaterynaKolomiiets <kateryna.kolomiiets.official@gmail.com>
2021-11-04 16:14:02 +0200

    save

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import { v4 } from "uuid";
+
+import "./App.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+import Filter from "./components/Filter";
+import ContactList from "./components/ContactList";
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: "",
+  };
+  // нужно ли удалять пустой contacts из LocalStorage?
+
+  componentDidMount() {
+    if (localStorage.getItem("contacts")) {
+      this.setState({
+        contacts: JSON.parse(localStorage.getItem("contacts")),
+      });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+      console.log(localStorage.getItem("contacts"));
+    }
+  }
+
+  formSubmitHandler = (data) => {
+    console.log(data);
+    if (
+      this.state.contacts.some(
+        (item) => item.name.toLowerCase() === data.name.toLowerCase()
+      )
+    ) {
+      alert(`${data.name} is already in your contacts`);
+      return;
+    }
+    this.setState((prevState) => ({
+      contacts: [...prevState.contacts, { id: v4(), ...data }],
+    }));
+  };
+
+  search = (e) => {
+    this.setState({
+      filter: e.target.value,
+    });
+  };
+
+  filter = () => {
+    return this.state.contacts.filter((item) =>
+      item.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
+  };
+
+  delete = (id) => {
+    this.setState({
+      contacts: this.state.contacts.filter((item) => item.id !== id),
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmitHandler} />
+        <h2>Contacts</h2>
+        <Filter onChange={this.search} />
+        <ContactList contacts={this.filter()} onDelete={this.delete} />
+      </div>
+    );
+  }
+}
+
+export default App;
